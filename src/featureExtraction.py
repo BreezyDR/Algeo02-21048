04ac0d5d3db767f689c80578f66fe51af7f7e3f2
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-02 12:05:05 +0700

    Update

--- src/featureExtraction.py
+++ src/featureExtraction.py
@@ -6,23 +6,23 @@ import random
 import os
 import matplotlib.pyplot as plt
 
+
 # Feature extractor
 def extract_features(image_path, vector_size=32):
     image = cv2.imread(image_path, imagemode='RGB')
     try:
         alg = cv2.KAZE_create()
-        
+
         kps = alg.detect(image)
-        
+
         kps = sorted(kps, key=lambda x: -x.response)[:vector_size]
-        
+
         kps, dsc = alg.compute(image, kps)
 
         dsc = dsc.flatten()
 
         needed_size = (vector_size * 64)
         if dsc.size < needed_size:
-           
             dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
     except cv2.error as e:
         print('Error: ', e)
