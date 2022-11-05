6158a5799eaf6e03c8b43dfe7979f56b6afc6be7
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-05 14:25:52 +0700

    Update gui.py
    Buat class MainWeb

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,16 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import Text, filedialog, BOTH, W, E, N, S
+from tkinter.ttk import Label, Style, Button, Frame
+import cv2
+
+class MainWeb(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.grid()
+        self.widget()
+        self.place()
+    def widget(self):
+        self.master.title("Face Recognition Kelompok 14 Kelas 1")
+        self.pack(fill=BOTH, expand=True)
