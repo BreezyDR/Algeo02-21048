3eb29cd0811ef28347fb9e0e13a975381b07dd86
fakihap <fakih.anugerah@gmail.com>
2022-11-12 18:35:56 +0700

    add: my attempt to port featureExtraction, Matcher to python3

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
 import src.eigenfaces as ef
-from src.matrix import Matrix, generateIdentityMatrix
+from src.Matrix import Matrix, generateIdentityMatrix
+
+from src.featureExtraction import batch_extractor, show_img
+from src.Matcher import Matcher
+
+import cv2
+import os
 
 #debug
 import random
 
 if __name__ == '__main__':
-    k = [[6,1,1], [4, -2, 5], [2,8,7]]
+    print(type('asd'))
+    k = [[6, 1, 1], [4, -2, 5], [2, 8, 7]]
     m = Matrix(k)
     
     print("determinan", m.getDeterminant())
 
     # ef.squashMat(k)
 
-    m.describe() # awal
+    m.describe()  # awal
 
     m.subtractBy(k)
 
-    m.describe() #setelah dikurangi dengan matrix dengan nilai yang sama
+    m.describe()  #setelah dikurangi dengan matrix dengan nilai yang sama
 
     print('\nadd images values matrix simulations\n')
 
     matOfMatrix = [Matrix() for i in range(10)]
     for i in range(10):
-            dummyMat = [[random.randint(1,10) for i in range(3)] for j in range(3)] # buat n matrix random
+        dummyMat = [[random.randint(1, 10) for k in range(3)] for j in range(3)]  # buat n matrix random
 
-            matOfMatrix[i].assign(dummyMat)
+        matOfMatrix[i].assign(dummyMat)
 
     summaryMat = Matrix(k)
     for i in range(10):
         summaryMat.addBy(matOfMatrix[i].getMatrix())
     
-    summaryMat.divideBy(scalar=10) # simulasi perata-rataan
+    summaryMat.divideBy(scalar=10)  # simulasi perata-rataan
 
     summaryMat.describe()
 
@@ -38,6 +45,32 @@ if __name__ == '__main__':
     toBeTransposed.describe()
     toBeTransposed.transpose()
     toBeTransposed.describe()
-    
 
+
+    # fE.batch_extractor('public/images/')
+    # k = fE.extract_features('public/images/basuta.jpg')
+    # print(len(k), len(k[0]))
+    # fE.show_img('public/images/basuta.jpg')
+
+    # l = cv2.imread('public/images/basuta.jpg')
+    # l = cv2.resize(l, (l.shape[1]//4, l.shape[0]//4))
+    # cv2.imshow('asdaf', l)
+    # cv2.waitKey()
     
+    images_path = 'public/images/'
+    files = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
+
+    batch_extractor(images_path)
+
+    ma = Matcher('features.pck')
+
+    for s in files:
+        print('Query image ==========================================')
+        # show_img(s)
+        names, match = ma.match(s, topn=3)
+        print('Result images ========================================')
+        for i in range(3):
+            # we got cosine distance, less cosine distance between vectors
+            # more they similar, thus we subtruct it from 1 to get match value
+            print('Match %s' % (1-match[i]))
+            show_img(os.path.join(images_path, names[i]))
--- src/featureExtraction.py
+++ src/featureExtraction.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
-import pickle
-import scipy
-import random
+# import random
 import os
 import matplotlib.pyplot as plt
+import pickle
+
+from typing import List
 
 
 # Feature extractor
-def extract_features(image_path, vector_size=32):
-    image = cv2.imread(image_path, imagemode='RGB')
+def extract_features(image_path : str, vector_size : int = 32) -> List[int]:
+    image = cv2.imread(image_path)
+    # regarding mode='RGB',
+    # 'mode' parameter is a scipy.misc.imread specific parameter
+    # which imread() method is removed on the 1.2.0 version of scipy
+    # the alternatives would be imageio.imread
+    # but i'll try using cv2's imread for now
+
     try:
         alg = cv2.KAZE_create()
 
@@ -19,8 +26,8 @@ def extract_features(image_path, vector_size=32):
 
         kps, dsc = alg.compute(image, kps)
 
-        dsc = dsc.flatten()
-
+        # dsc = dsc.flatten()
+ 
         needed_size = (vector_size * 64)
         if dsc.size < needed_size:
             dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
@@ -30,7 +37,7 @@ def extract_features(image_path, vector_size=32):
     return dsc
 
 
-def batch_extractor(images_path, pickled_db_path="features.pck"):
+def batch_extractor(images_path : str, pickled_db_path : str ="features.pck") -> None :
     files = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
 
     result = {}
@@ -39,5 +46,18 @@ def batch_extractor(images_path, pickled_db_path="features.pck"):
         name = f.split('/')[-1].lower()
         result[name] = extract_features(f)
 
-    with open(pickled_db_path, 'w') as fp:
+        print(np.array(result[name]).shape, name, 'sizeof')
+
+    with open(pickled_db_path, 'wb') as fp:
+        # 'wb' is used instead of 'w' since it produces an error
+        # see : https://stackoverflow.com/questions/13906623/using-pickle-dump-typeerror-must-be-str-not-bytes
         pickle.dump(result, fp)
+
+
+# show an image with matplotlib
+# caution: opencv color format is BGR, instead of plt's RGB, hence the need for cvtColor
+def show_img(path):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.show()
