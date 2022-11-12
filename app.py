e556a19d97566e70ee0bae49d4c33e800e356b6c
fakihap <fakih.anugerah@gmail.com>
2022-11-12 23:59:54 +0700

    update: app.py rework
    image feature extraction method rework
    instead of using KAGE method and scipy, we will be using self-made methods

--- app.py
+++ app.py
@@ -1,50 +1,51 @@
 import src.eigenfaces as ef
 from src.Matrix import Matrix, generateIdentityMatrix
 
-from src.featureExtraction import batch_extractor, show_img
-from src.Matcher import Matcher
+# from src.featureExtraction import batch_extractor, show_img
+# from src.Matcher import Matcher
 
 import cv2
+import numpy as np
 import os
 
 #debug
 import random
 
 if __name__ == '__main__':
-    print(type('asd'))
-    k = [[6, 1, 1], [4, -2, 5], [2, 8, 7]]
-    m = Matrix(k)
+    # print(type('asd'))
+    # k = [[6, 1, 1], [4, -2, 5], [2, 8, 7]]
+    # m = Matrix(k)
     
-    print("determinan", m.getDeterminant())
+    # print("determinan", m.getDeterminant())
 
-    # ef.squashMat(k)
+    # # ef.squashMat(k)
 
-    m.describe()  # awal
+    # m.describe()  # awal
 
-    m.subtractBy(k)
+    # m.subtractBy(k)
 
-    m.describe()  #setelah dikurangi dengan matrix dengan nilai yang sama
+    # m.describe()  #setelah dikurangi dengan matrix dengan nilai yang sama
 
-    print('\nadd images values matrix simulations\n')
+    # print('\nadd images values matrix simulations\n')
 
-    matOfMatrix = [Matrix() for i in range(10)]
-    for i in range(10):
-        dummyMat = [[random.randint(1, 10) for k in range(3)] for j in range(3)]  # buat n matrix random
+    # matOfMatrix = [Matrix() for i in range(10)]
+    # for i in range(10):
+    #     dummyMat = [[random.randint(1, 10) for k in range(3)] for j in range(3)]  # buat n matrix random
 
-        matOfMatrix[i].assign(dummyMat)
+    #     matOfMatrix[i].assign(dummyMat)
 
-    summaryMat = Matrix(k)
-    for i in range(10):
-        summaryMat.addBy(matOfMatrix[i].getMatrix())
+    # summaryMat = Matrix(k)
+    # for i in range(10):
+    #     summaryMat.addBy(matOfMatrix[i].getMatrix())
     
-    summaryMat.divideBy(scalar=10)  # simulasi perata-rataan
+    # summaryMat.divideBy(scalar=10)  # simulasi perata-rataan
 
-    summaryMat.describe()
+    # summaryMat.describe()
 
-    toBeTransposed = Matrix(matrix=[[1, 2, 3], [4, 5, 6]])
-    toBeTransposed.describe()
-    toBeTransposed.transpose()
-    toBeTransposed.describe()
+    # toBeTransposed = Matrix(matrix=[[1, 2, 3], [4, 5, 6]])
+    # toBeTransposed.describe()
+    # toBeTransposed.transpose()
+    # toBeTransposed.describe()
 
 
     # fE.batch_extractor('public/images/')
@@ -52,25 +53,38 @@ if __name__ == '__main__':
     # print(len(k), len(k[0]))
     # fE.show_img('public/images/basuta.jpg')
 
-    # l = cv2.imread('public/images/basuta.jpg')
-    # l = cv2.resize(l, (l.shape[1]//4, l.shape[0]//4))
-    # cv2.imshow('asdaf', l)
+
+
+    # AN ATTEMPT TO 100% USE NUMPY.ARRAY TO SOLVE FOR EVERYTHING
+    # WE MIGHT MAKE AN numpy.array API WRAPPER INSTEAD IN THE FUTURE
+
+    # cv2.namedWindow('result', cv2.WINDOW_FREERATIO)
+    images_path = 'public/images/'
+    files_path = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
+    files = [cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in files_path]
+    
+    for i in range(len(files)):
+        files[i] = cv2.resize(files[i], (files[i].shape[1]//4, files[i].shape[0]//4)) # faster calculation
+        files[i] = np.array(files[i]).reshape(-1, 1)
+        
+    filesMeaned = np.mean(files, axis=0) # psi
+    
+    filesDiff = [files[i] - filesMeaned for i in range(len(files))] # a
+
+    
+
+
+
+    # print(files)
+    # print(np.mean(files, axis=0))
+    
+    # l = cv2.imread('public/images/basuta.jpg', cv2.IMREAD_GRAYSCALE)
+    # l = cv2.resize(l, (l.shape[1]//4, l.shape[0]//4)) # faster calculation
+    
+    
+
+    
+    # print(np.array(l).shape)
+    # cv2.imshow('result', l)
     # cv2.waitKey()
     
-    images_path = 'public/images/'
-    files = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
-
-    batch_extractor(images_path)
-
-    ma = Matcher('features.pck')
-
-    for s in files:
-        print('Query image ==========================================')
-        # show_img(s)
-        names, match = ma.match(s, topn=3)
-        print('Result images ========================================')
-        for i in range(3):
-            # we got cosine distance, less cosine distance between vectors
-            # more they similar, thus we subtruct it from 1 to get match value
-            print('Match %s' % (1-match[i]))
-            show_img(os.path.join(images_path, names[i]))
