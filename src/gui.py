f21f69d820eca06a5c879be422b60a9e329c20c7
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-15 12:01:59 +0700

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -4,7 +4,7 @@ from tkinter import Text, filedialog, BOTH, W, E, N, S
 from tkinter.ttk import Label, Style, Button, Frame
 import cv2
 
-class MainWeb(tk.Frame):
+class MainGUI(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -12,4 +12,14 @@ class MainWeb(tk.Frame):
         self.widget()
         self.place()
     def widget(self):
+        self.pack(fill=BOTH, expand=True)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
         
+
+
+run = tk.Tk()
+run.title("Face Recognition")
+run.geometry("400x300 + 300x300")
+gui = MainGUI(master=run)
+gui.mainloop()
