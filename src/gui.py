e6eb0793de9879587841bd60ef3cfb229ea65031
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-19 18:43:54 +0700

    change gui

--- src/gui.py
+++ src/gui.py
@@ -2,24 +2,37 @@ import tkinter as tk
 from tkinter import *
 from tkinter import Text, filedialog, BOTH, W, E, N, S
 from tkinter.ttk import Label, Style, Button, Frame
+from PIL import Image, ImageTk
 import cv2
 
-class MainGUI(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.grid()
-        self.widget()
-        self.place()
-    def widget(self):
-        self.pack(fill=BOTH, expand=True)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=4)
-        
+root = Tk()
+root.title("Face Recognition - Reigenface")
+
+# Styles
+
+s = Style()
+s.configure('Mainframe.Tframe', background = '#d6f3ff')
+
+# Images
 
 
-run = tk.Tk()
-run.title("Face Recognition")
-run.geometry("400x300 + 300x300")
-gui = MainGUI(master=run)
-gui.mainloop()
+# Frames
+
+mainframe = Frame(root, width=1600, height=800, style='Mainframe.Tframe')
+mainframe.grid(row=0, column=0, sticky='NSEW')
+
+topframe = Frame(mainframe)
+topframe.grid(row=0, column=0, sticky='NSEW', columnspan=2)
+
+uploadphotoFrame = Frame(mainframe, width=300)
+uploadphotoFrame.grid(row=1, column=0, padx=3, pady=3, sticky='NSEW')
+
+conversionFrame = Frame(mainframe, width=1200)
+conversionFrame.grid(row=1, column=1, padx=3, pady=3, sticky='NSEW')
+
+
+# Conversion Frame section
+
+
+# Execution
+root.mainloop()
