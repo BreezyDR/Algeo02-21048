70e9740be33891f11e9f2653099e1fa1f2eee06b
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-19 21:37:19 +0700

    Update gui button and label

--- src/gui.py
+++ src/gui.py
@@ -12,6 +12,8 @@ root.title("Face Recognition - Reigenface")
 
 s = Style()
 s.configure('Mainframe.Tframe', background = '#d6f3ff')
+s.configure('SubTitle.TLabel', font = ('Helvetica', 14, 'bold'), foreground = 'black', padding = (5, 5, 5, 5), width = 20)
+s.configure('Upload.TButton', font = ('arial', 10, 'underline'), foreground = 'blue')
 
 # Images
 
@@ -29,6 +31,22 @@ uploadphotoFrame.grid(row=1, column=0, padx=3, pady=3, sticky='NSEW')
 
 conversionFrame = Frame(mainframe, width=1200)
 conversionFrame.grid(row=1, column=1, padx=3, pady=3, sticky='NSEW')
+
+
+# Upload Photo & Database section
+uploaddatatestLabel = Label(uploadphotoFrame, text="Masukkan data test Anda", style= 'SubTitle.TLabel')
+uploaddatatestLabel.grid(row=0, column=0, sticky= 'WE')
+uploaddatatestLabel.configure(anchor='center')
+
+datatestButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton')
+datatestButton.grid(row=1, column=0, sticky='WE')
+
+uploadtestphotoLabel = Label(uploadphotoFrame, text="Masukkan image Anda", style= 'SubTitle.TLabel')
+uploadtestphotoLabel.grid(row=2, column=0, sticky= 'WE')
+uploadtestphotoLabel.configure(anchor='center')
+
+testphotoButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton')
+testphotoButton.grid(row=3, column=0, sticky='WE')
 
 
 # Conversion Frame section
