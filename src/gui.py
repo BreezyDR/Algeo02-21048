348b95c6f29de38a98025915e79b4fd50c00690d
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-19 23:27:07 +0700

    uploadimage, testimage

--- src/gui.py
+++ src/gui.py
@@ -1,21 +1,34 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import Text, filedialog, BOTH, W, E, N, S
+from tkinter.filedialog import askopenfile
 from tkinter.ttk import Label, Style, Button, Frame
 from PIL import Image, ImageTk
 import cv2
 
 root = Tk()
 root.title("Face Recognition - Reigenface")
+root.geometry("1600x800")
 
 # Styles
 
 s = Style()
 s.configure('Mainframe.Tframe', background = '#d6f3ff')
-s.configure('SubTitle.TLabel', font = ('Helvetica', 14, 'bold'), foreground = 'black', padding = (5, 5, 5, 5), width = 20)
+s.configure('SubTitle.TLabel', font = ('Helvetica', 14), foreground = 'black', padding = (5, 5, 5, 5), width = 20)
 s.configure('Upload.TButton', font = ('arial', 10, 'underline'), foreground = 'blue')
+s.configure('Title.TLabel', font=('Helvetica', 20, 'bold'), foreground = 'black', padding = (5, 5, 5, 5))
 
-# Images
+
+# Upload image
+def upload_image():
+    types = [('Jpg Files', '*.jpg'),
+    ('PNG Files','*.png'), ('Jpeg Files', '*.jpeg')]
+    filename = filedialog.askopenfilename(multiple=False, filetypes=types)
+    testphoto = Image.open(filename)
+    testphoto = testphoto.resize((400,400), Image.ANTIALIAS)
+    testphoto = ImageTk.PhotoImage(testphoto)
+    e1 = Label(conversionFrame, image=testphoto)
+    e1.grid(row=1,column=0,sticky='WE')
 
 
 # Frames
@@ -32,24 +45,37 @@ uploadphotoFrame.grid(row=1, column=0, padx=3, pady=3, sticky='NSEW')
 conversionFrame = Frame(mainframe, width=1200)
 conversionFrame.grid(row=1, column=1, padx=3, pady=3, sticky='NSEW')
 
+# Top Frame Section
+titleLabel = Label(topframe, text="Face Recognition - Reigenface", style='Title.TLabel')
+titleLabel.place(relx=0.5,rely=0.5,anchor=CENTER)
 
 # Upload Photo & Database section
 uploaddatatestLabel = Label(uploadphotoFrame, text="Masukkan data test Anda", style= 'SubTitle.TLabel')
 uploaddatatestLabel.grid(row=0, column=0, sticky= 'WE')
-uploaddatatestLabel.configure(anchor='center')
 
-datatestButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton')
+datatestButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton',width=10)
 datatestButton.grid(row=1, column=0, sticky='WE')
 
 uploadtestphotoLabel = Label(uploadphotoFrame, text="Masukkan image Anda", style= 'SubTitle.TLabel')
 uploadtestphotoLabel.grid(row=2, column=0, sticky= 'WE')
-uploadtestphotoLabel.configure(anchor='center')
 
-testphotoButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton')
+testphotoButton = Button(uploadphotoFrame, text = 'Choose File', style='Upload.TButton',width=10,command=lambda:upload_image())
 testphotoButton.grid(row=3, column=0, sticky='WE')
+
+resultLabel = Label(uploadphotoFrame, text='Result', style='Upload.TButton')
+resultLabel.grid(row=4, column=0, sticky='WE')
 
 
 # Conversion Frame section
+
+testimageLabel = Label(conversionFrame, text='Test Image', style= 'SubTitle.TLabel')
+testimageLabel.grid(row=0, column=0, sticky='WE')
+
+closestResultLabel = Label(conversionFrame, text='Closest Result', style= 'SubTitle.TLabel')
+closestResultLabel.grid(row=0, column=1, sticky='WE')
+
+executionLabel = Label(conversionFrame, text='Execution time:', style='SubTitle.Tlabel')
+executionLabel.grid(row=2, column=0, sticky='WE')
 
 
 # Execution
