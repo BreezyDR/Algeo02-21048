c53f22f002aef92a0570eefb48ff588b8be050ec
fakihap <fakih.anugerah@gmail.com>
2022-11-20 13:50:52 +0700

    fix: wrong algorithm in util, json file integration

--- src/file.py
+++ src/file.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import os
 
+import json
+
+# read image from files
 def readFolder(path: str) -> tuple[np.ndarray, str, int]:
     files_path = [os.path.join(path, p) for p in sorted(os.listdir(path))]
     files = np.array([cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in files_path])
@@ -13,3 +16,22 @@ def readFile(exact_path: str) -> np.ndarray:
 
     return file
 
+
+
+# dump calculated data into json
+saves_path = "src/json/" # should be static
+def readDataAsArray(exact_path: str) -> tuple[str, np.ndarray]:
+    data = json.loads(os.path.join(saves_path, exact_path))
+
+    return data["name"], np.ndarray(data["data"])
+
+ # need optimization, preferrably go make it into int instead of float
+def writeArrayAsData(exact_path: str, data: np.ndarray, name: str = 'written by writeArrayAsData') -> None:
+    json_data = json.dumps({
+                    "name" : name,
+                    "data" : np.array(data).tolist()
+                })
+
+    with open(exact_path, 'w') as file:
+        file.write(json_data)
+    
--- src/utility.py
+++ src/utility.py
@@ -3,10 +3,12 @@ import numpy as np
 
 from sklearn.preprocessing import normalize
 
+
+# pass check
 def resize(arr : np.ndarray, size : tuple[int] | int) -> np.ndarray:
     if not type(size) == int :
         arr = cv2.resize(arr, size, interpolation=cv2.INTER_CUBIC) #here we use cv2.resize instead of np.resize,
-                                                                                #since we need to interpolate missing values
+                                                                  #since we need to interpolate missing values
     else :
         minDim = min(arr.shape[0], arr.shape[1])
         
@@ -14,8 +16,10 @@ def resize(arr : np.ndarray, size : tuple[int] | int) -> np.ndarray:
     
     return arr
 
+# fixed
 def unflatten(arr : np.ndarray) -> np.ndarray:
-    return resize(arr, (int(arr.shape[0]**.5), int(arr.shape[0]**.5)))
+    # return resize(arr, (int(arr.shape[0]**.5), int(arr.shape[0]**.5)))
+    return arr.reshape(int(arr.shape[0]**.5), -1)
 
 def magiclyNormalize(arr : np.ndarray) -> np.ndarray:
     print('from mag', arr.shape)
@@ -28,8 +32,8 @@ def magiclyDisplay(arr : np.ndarray, name : str = 'from utility.py') -> None:
 def normalizeNP(arr : np.ndarray) -> np.ndarray:
     sum = np.sum(arr)
 
-    print(arr.shape)
+    mean = np.mean(arr)
 
-    arr = np.array([[np.float64(arr[j][i]/sum) for i in range(arr.shape[1])] for j in range(arr.shape[0])])
+    arr = np.array([[np.float64(abs(arr[j][i]/sum*100/mean)) for i in range(arr.shape[1])] for j in range(arr.shape[0])])
 
     return arr
