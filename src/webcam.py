901e57d478cd371fff4db4d02efb4a5332fee1f4
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-11-20 07:10:00 +0700

    Update webcam.py

--- src/webcam.py
+++ src/webcam.py
@@ -19,4 +19,68 @@ class FaceRecognition:
     known_encodings = []
     known_names = []
     is_current_frame = True
+
+    def __init__(self):
+        self.encode_faces()
+
+    def encode_faces(self):
+        for image in os.listdir('test'):
+            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_encodings.append(face_encoding)
+            self.known_names.append(image)
+        print(self.known_names)
+    
+    def run_program(self):
+        capturevideo = cv2.VideoCapture(0)
+
+        if not capturevideo.isOpened():
+            sys.exit('Video source not found!')
+
+        while True:
+            ret, frame = capturevideo.read()
+
+            if self.is_current_frame:
+                miniframe = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                miniframe_rgb = miniframe[:, :, ::-1]
+                self.locations = face_recognition.face_locations(miniframe_rgb)
+                self.encodings = face_recognition.face_encodings(miniframe_rgb, self.locations)
+
+                self.names = []
+                for encodings in self.encodings:
+                    matching = face_recognition.compare_faces(self.known_encodings, encodings)
+                    name = "Unknown"
+                    confidence = '???'
+                    distance = face_recognition.face_distance(self.known_encodings, encodings)
+                    fitting_match = np.argmin(distance)
+                    if matching[fitting_match]:
+                        name = self.known_names[fitting_match]
+                        confidence = face_limit(distance[fitting_match])
+                    self.names.append(f'{name} ({confidence})')
+            self.is_current_frame = not self.is_current_frame
+
+            for (top, right, bottom, left), name in zip(self.locations, self.names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 1)
+            cv2.imshow('Face Recognition', frame)
+
+            # Untuk berhenti merekam, klik 'Q' di keyboard
+            if cv2.waitKey(1) == ord('q'):
+                break
+        capturevideo.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    webcam = FaceRecognition()
+    webcam.run_program()
+
+
     
